945b44b764c3fdbf9051ea1212afe26ec8f381ec
Sanithu <it25101412@my.sliit.lk>
2026-03-05 17:20:17 +0530

    Project Start

---
+++ src/main/java/com/example/vehicalrentalserviceplatform/VehicalRentalServicePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehicalrentalserviceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicalRentalServicePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicalRentalServicePlatformApplication.class, args);
+    }
+
+}
