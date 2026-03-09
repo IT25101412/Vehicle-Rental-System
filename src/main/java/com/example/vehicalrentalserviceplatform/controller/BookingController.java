d840808bb2a8e2c60da566814362889d813542db
Sanithu <it25101412@my.sliit.lk>
2026-03-09 13:33:02 +0530

    coded the BookingController class

--- src/main/java/com/example/vehicalrentalserviceplatform/controller/BookingController.java
+++ src/main/java/com/example/vehicalrentalserviceplatform/controller/BookingController.java
@@ -1,4 +1,30 @@
 package com.example.vehicalrentalserviceplatform.controller;
 
-public class BookingController {
+import com.example.vehicalrentalserviceplatform.model.Booking;
+import com.example.vehicalrentalserviceplatform.service.BookingService;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/createBooking")
+
+public class BookingController extends HttpServlet{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  IOException {
+
+        String name = request.getParameter("customerUsername");
+        String vehicleId = request.getParameter("vehicleId");
+        String startDate = request.getParameter("startDate");
+        String returnDate = request.getParameter("returnDate");
+
+        Booking newBooking = new Booking(name,vehicleId,startDate,returnDate,"Pending");
+
+        BookingService service = new BookingService();
+        service.createBooking(newBooking);
+
+        response.sendRedirect("reservationHistory.jsp");
+    }
 }
