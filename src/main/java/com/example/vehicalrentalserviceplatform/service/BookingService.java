534f67ba877ce18884784eb35d09eb605c5fdb9f
Sanithu <it25101412@my.sliit.lk>
2026-05-11 19:14:02 +0530

    fixed some accidental deletions of Booking.java and BookingService.java

---
+++ src/main/java/com/example/vehicalrentalserviceplatform/service/BookingService.java
@@ -0,0 +1,137 @@
+package com.example.vehicalrentalserviceplatform.service;
+
+import com.example.vehicalrentalserviceplatform.model.Booking;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDate;
+
+public class BookingService {
+
+    private static final String FILE_NAME = "booking.txt";
+
+    public boolean isVehicleAvailable(String vehicleId, String requestedStartDate, String requestedReturnDate){
+
+        List<Booking> activeBookings = getAllBookings();
+
+        LocalDate reqStart = LocalDate.parse(requestedStartDate);
+        LocalDate reqReturn = LocalDate.parse(requestedReturnDate);
+
+        for(Booking x : activeBookings){
+            if (x.getVehicleId().equals(vehicleId) && !x.getBookingStatus().equals("Cancelled")){
+
+                LocalDate bookedStart = LocalDate.parse(x.getStartDate());
+                LocalDate bookedReturn = LocalDate.parse(x.getReturnDate());
+
+
+                if (!reqStart.isAfter(bookedReturn) && !reqReturn.isBefore(bookedStart)) {
+                    return false; // Car is taken during these dates
+                }
+            }
+        }
+        return true; // No overlaps found, car is yours!
+    }
+
+    public  void  createBooking(Booking newBooking){
+
+        if(!isVehicleAvailable(newBooking.getVehicleId(),newBooking.getStartDate(),newBooking.getReturnDate())){
+            System.out.println("Error: Vehicle is already booked for those dates.");
+            return;
+        }
+
+        try(FileWriter fileWriter = new FileWriter(FILE_NAME, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            PrintWriter out = new PrintWriter(bufferedWriter)){
+
+            out.println(newBooking.toString());
+            System.out.println("Booking Successful");
+
+        }catch(IOException e){
+            System.out.println("An error occurred");
+            e.printStackTrace();
+        }
+    }
+
+    public List<Booking> getAllBookings(){
+        List<Booking> bookingList = new ArrayList<>();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE_NAME))){
+            String line;
+            while ((line = bufferedReader.readLine()) != null){
+                String[] data = line.split(",");
+
+                if(data.length == 6){
+                    Booking booking = new Booking(data[0],data[1],data[2],data[3],data[4],data[5]);
+                    bookingList.add(booking);
+                }
+            }
+        }catch (IOException e){
+            System.out.println("No existing bookings found or error reading file.");
+        }
+        return bookingList;
+
+    }
+
+    public List<Booking> getBookingsByCustomer(String customerName) {
+        List<Booking> allBookings = getAllBookings();
+
+        List<Booking> userBookings = new ArrayList<>();
+
+        for (Booking x : allBookings) {
+            if (x.getCustomerName().equalsIgnoreCase(customerName)) {
+                userBookings.add(x);
+            }
+        }
+
+        return userBookings;
+    }
+
+    private void overWriteBookingFile(List<Booking> updatedBookings){
+
+        try(FileWriter fileWriter = new FileWriter(FILE_NAME,false);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            PrintWriter out = new PrintWriter(bufferedWriter)){
+
+            for (Booking x : updatedBookings){
+                out.println(x.toString());
+            }
+        }catch (IOException e){
+            System.out.println("Error updating the bookings file");
+            e.printStackTrace();
+        }
+
+    }
+
+    public void updateBooking(String transactionId,String newVehicleID, String newStartDate, String newReturnDate, String newStatus){
+        List<Booking> allBookings = getAllBookings();
+        boolean isUpdated = false;
+
+        for (Booking x : allBookings){
+            if(x.getTransactionId().equals(transactionId)){
+                x.setVehicleId(newVehicleID);
+                x.setStartDate(newStartDate);
+                x.setReturnDate(newReturnDate);
+                x.setBookingStatus(newStatus);
+                isUpdated = true;
+                break;
+            }
+        }
+
+        if (isUpdated){
+            overWriteBookingFile(allBookings);
+            System.out.println("Booking updated successfully");
+        }else {
+            System.out.println("Error: Booking updated failed");
+        }
+    }
+
+    public void deleteBooking(String transactionId){
+        List<Booking> allBookings = getAllBookings();
+
+        boolean isRemoved = allBookings.removeIf(x -> x.getTransactionId().equals(transactionId) && x.getBookingStatus().equals("Pending"));
+
+        if (isRemoved){
+            overWriteBookingFile(allBookings);
+        }
+    }
+}
